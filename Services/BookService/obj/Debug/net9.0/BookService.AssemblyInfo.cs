f56fd9f502d37bdebaac80c40f17c8dfc845e073
nevanftrc <143558001+nevanftrc@users.noreply.github.com>
2025-04-30 15:18:24 +0200

    nice

--- Services/BookService/obj/Debug/net9.0/BookService.AssemblyInfo.cs
+++ Services/BookService/obj/Debug/net9.0/BookService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17ab35394958d129f2474295f1003484ec175055")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eabaa223e7c69cf0e5a8e378b6ca361a73d611e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
