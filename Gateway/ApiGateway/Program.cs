b0aa751d0ed9e8075b97a1b50910b4c7e79e43c7
nevanftrc <143558001+nevanftrc@users.noreply.github.com>
2025-05-02 08:07:35 +0200

    fix

--- Gateway/ApiGateway/Program.cs
+++ Gateway/ApiGateway/Program.cs
@@ -1,6 +1,17 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Cargar ocelot.json
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+// Agregar Ocelot
+builder.Services.AddOcelot();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
+await app.UseOcelot();
 
 app.Run();
